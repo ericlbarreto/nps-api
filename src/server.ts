38e16aee5a1ca76c7d866fb83b5174652c58dd2e
Eric Barreto <ericbarreto521@gmail.com>
2024-05-06 22:37:12 -0300

    setup: criando gitignore e server.ts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (request, response) => {
+
+    return response.json({message: "Teste mensagem NLW"})
+})
+
+app.post("/", (request, response) => {
+    return response.json({message: "Dados salvos com sucesso"})
+})
+
+app.listen(3333, () => console.log('Server is running!'));
